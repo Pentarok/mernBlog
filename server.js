9a8770dff731c65b612e08659288b16ecfe924da
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-09-28 02:06:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const multerS3 = require('multer-s3');  // Import multer S3
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://front-blog-eta.vercel.app',
     methods: ["GET,POST,PUT,DELETE"],
     credentials: true
 }));
