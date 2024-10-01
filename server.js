f6acc65ca2884c61ac9f9265d853398d140050f4
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-10-01 08:03:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -450,7 +450,7 @@ app.post('/login', async (req, res) => {
 
           res.cookie('token', token, { 
   httpOnly: true, 
-  secure: process.env.NODE_ENV === 'production', 
+  secure:true, 
   sameSite: 'None'
 });
 
